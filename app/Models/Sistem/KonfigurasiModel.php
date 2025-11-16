a1c2fb27475b2d72ffb043fac8a09565cec39faf
Miftahul Haq <ciftah12@gmail.com>
2025-11-16 09:04:50 +0700

    Tidy up2

--- app/Models/Sistem/KonfigurasiModel.php
+++ app/Models/Sistem/KonfigurasiModel.php
@@ -7,71 +7,71 @@ use Illuminate\Support\Facades\Cache;
 
 class KonfigurasiModel extends Model
 {
-  /**
-   * nama tabel model ini.
-   *
-   * @var string
-   */
-  protected $table = 'konfigurasis';
-  /**
-   * primary key tabel ini.
-   *
-   * @var string
-   */
-  protected $primaryKey = 'id';
-  /**
-   * The attributes that are mass assignable.
-   *
-   * @var array
-   */
-  protected $fillable = [
-    'id',
-    'config_group',
-    'config_key',
-    'config_value',
-    'value_1',
-    'value_2',
-    'value_3',
-    'value_4',
-    'value_5'
-  ];
+    /**
+     * nama tabel model ini.
+     *
+     * @var string
+     */
+    protected $table = 'konfigurasis';
+    /**
+     * primary key tabel ini.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'config_group',
+        'config_key',
+        'config_value',
+        'value_1',
+        'value_2',
+        'value_3',
+        'value_4',
+        'value_5'
+    ];
 
-  /**
-   * Enable auto_increment.
-   *
-   * @var string
-   */
-  public $incrementing = true;
+    /**
+     * Enable auto_increment.
+     *
+     * @var string
+     */
+    public $incrementing = true;
 
-  /**
-   * activated timestamps.
-   *
-   * @var string
-   */
-  public $timestamps = true;
+    /**
+     * activated timestamps.
+     *
+     * @var string
+     */
+    public $timestamps = true;
 
-  /**
-   * Digunakan untuk  menyimpan ke cache.
-   *
-   */
-  public static function toCache()
-  {
-    $konfigs = KonfigurasiModel::all()->pluck('config_value', 'config_key')->toArray();
-    Cache::put('konfigs', $konfigs);
-  }
+    /**
+     * Digunakan untuk  menyimpan ke cache.
+     *
+     */
+    public static function toCache()
+    {
+        $konfigs = KonfigurasiModel::all()->pluck('config_value', 'config_key')->toArray();
+        Cache::put('konfigs', $konfigs);
+    }
 
-  /**
-   * Digunakan untuk  melakukan refresh cache.
-   *
-   */
-  public static function refreshCache()
-  {
-    Cache::flush();
-    Cache::forget('konfigs');
-    Cache::rememberForever('konfigs', function () {
-      return self::pluck('config_value', 'config_key');
-    });
-  }
+    /**
+     * Digunakan untuk  melakukan refresh cache.
+     *
+     */
+    public static function refreshCache()
+    {
+        Cache::flush();
+        Cache::forget('konfigs');
+        Cache::rememberForever('konfigs', function () {
+            return self::pluck('config_value', 'config_key');
+        });
+    }
 
   // /**
   //  * digunakan untuk  memanggil data cache.
@@ -100,13 +100,13 @@ class KonfigurasiModel extends Model
   //   Cache::flush();
   // }
 
-  /**
-   * Digunakan untuk mengambil nama tabel model ini.
-   *
-   * @return string
-   */
-  public static function getTableName()
-  {
-    return with(new static)->getTable();
-  }
+    /**
+     * Digunakan untuk mengambil nama tabel model ini.
+     *
+     * @return string
+     */
+    public static function getTableName()
+    {
+        return with(new static)->getTable();
+    }
 }
