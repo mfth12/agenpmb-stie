6f3f21fcba45443f7079a242cb7872392b640d52
Miftahul Haq <ciftah12@gmail.com>
2025-11-23 22:38:16 +0700

    Create KonfigurasiController.php

---
+++ app/Http/Controllers/Sistem/KonfigurasiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Sistem;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KonfigurasiController extends Controller
+{
+    //
+}
