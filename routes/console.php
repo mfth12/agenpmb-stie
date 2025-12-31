97bbd6b86d47f403423f2c99c328a3ccfc7fb271
Miftahul Haq <ciftah12@gmail.com>
2025-12-31 11:37:20 +0700

    Update console.php
    using supervisor instead

--- routes/console.php
+++ routes/console.php
@@ -10,13 +10,16 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-//job queue default seperti email, media library, rutin
-Schedule::command('queue:work --stop-when-empty --queue=default')
-    ->everyMinute();
+// //job queue default seperti email, media library, rutin
+// Schedule::command('queue:work --stop-when-empty --queue=default')
+//     ->everyMinute();
 
-// job whatsapp
-Schedule::command('queue:work --stop-when-empty --queue=whatsapp')
-    ->withoutOverlapping()
-    ->everyMinute();
-// ->sendOutputTo(storage_path() . '/logs/whatsapp.log')
-// --stop-when-empty
+// // job whatsapp
+// Schedule::command('queue:work --stop-when-empty --queue=whatsapp')
+//     ->withoutOverlapping()
+//     ->everyMinute();
+// // no use
+// // ->sendOutputTo(storage_path() . '/logs/whatsapp.log')
+// // --stop-when-empty
+
+// php artisan queue:work --queue=default,whatsapp --sleep=1 --tries=3
